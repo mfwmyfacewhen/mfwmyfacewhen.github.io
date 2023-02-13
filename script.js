56556d2f94a85b2d867740519a2eb6d410197aa2
mfwmyfacewhen <94880365+mfwmyfacewhen@users.noreply.github.com>
2023-02-13 00:04:23 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+function getIP(json) {
+        document.write("<h1> Your public IP address is " + json.ip + "</h1>");
+      }
+      function osbrowserfunction () {
+            let ua = navigator.userAgent;
+            let finalOs = "";
+            let finalBrowser = "";
+
+            if (ua.includes('Windows')){
+                finalOs="Windows";
+            }
+            else if (ua.includes('Android')){
+                finalOs="Android";
+            }
+            else if (ua.includes('Mac') && navigator.maxTouchPoints > 1){
+                finalOs="iOS";
+            }
+            else if (ua.includes('Mac')){
+                finalOs="MacOS";
+            }
+            else if (ua.includes('CrOS')){
+                finalOs="ChromeOS"
+            }
+            else if (ua.includes('Linux')){
+                finalOs="Linux";
+            }
+            if (ua.includes('OPR')){
+                finalBrowser="Opera";
+            }
+            else if (navigator.brave !== undefined){
+                finalBrowser = "Brave"
+            }
+            else if (ua.includes('Firefox')){
+                 finalBrowser="Firefox";
+             }
+            else if (ua.includes('Edg')){
+                 finalBrowser="Edge";
+             }
+            else if (ua.includes('Chrome')){
+                finalBrowser="Chrome";
+             }
+            else if (ua.includes('Safari')){
+                 finalBrowser="Safari";
+             }
+            document.write("<h1> You are running " + finalBrowser + " on " + finalOs + "</h1>");
+        }
+        osbrowserfunction();
+        let x = localStorage.getItem("visited");
+        if (x == "true") {
+            document.write("<h1>Welcome back</h1>");
+        }
+        localStorage.setItem("visited", "true");
