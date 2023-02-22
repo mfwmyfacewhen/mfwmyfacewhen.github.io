061458ea8fbebe728e0158db7caeb5695000392e
mfwmyfacewhen <94880365+mfwmyfacewhen@users.noreply.github.com>
2023-02-21 23:51:21 -0600

    fix ublock detection

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
-try {
-        function getIP(json) {
-        
-                document.write("<h1> Your public IP address is " + json.ip + "</h1>");
-        }
+function getIP(json) {
+        document.write("<h1> Your public IP address is " + json.ip + "</h1>");
 }
-catch (error) {
-        document.write("<h1>Are you using UBlock? Naughty naughty</h1>")       
-}
+if (!document.body.innerText.includes('.'))
+        document.write("<h1>Are you using UBlock? Naughty naughty</h1>");      
 function osbrowserfunction () {
-    let ua = navigator.userAgent;
-    let finalOs = "";
-    let finalBrowser = "";
+        let ua = navigator.userAgent;
+        let finalOs = "";
+        let finalBrowser = "";
 
-    if (ua.includes('Windows')){
-        finalOs="Windows";
-    }
-    else if (ua.includes('Android')){
-        finalOs="Android";
-    }
-    else if (ua.includes('Mac') && navigator.maxTouchPoints > 1){
-        finalOs="iOS";
-    }
-    else if (ua.includes('Mac')){
-        finalOs="MacOS";
-    }
-    else if (ua.includes('CrOS')){
-        finalOs="ChromeOS"
-    }
-    else if (ua.includes('Linux')){
-        finalOs="Linux";
-    }
-    if (ua.includes('OPR')){
-        finalBrowser="Opera";
-    }
-    else if (navigator.brave !== undefined){
-        finalBrowser = "Brave"
-    }
-    else if (ua.includes('Firefox')){
-         finalBrowser="Firefox";
-     }
-    else if (ua.includes('Edg')){
-         finalBrowser="Edge";
-     }
-    else if (ua.includes('Chrome')){
-        finalBrowser="Chrome";
-     }
-    else if (ua.includes('Safari')){
-         finalBrowser="Safari";
-     }
-    document.write("<h1> You are running " + finalBrowser + " on " + finalOs + "</h1>");
+        if (ua.includes('Windows')){
+                finalOs="Windows";
+        }
+        else if (ua.includes('Android')){
+                finalOs="Android";
+        }
+        else if (ua.includes('Mac') && navigator.maxTouchPoints > 1){
+                finalOs="iOS";
+        }
+        else if (ua.includes('Mac')){
+                finalOs="MacOS";
+        }
+        else if (ua.includes('CrOS')){
+                finalOs="ChromeOS"
+        }
+        else if (ua.includes('Linux')){
+                finalOs="Linux";
+        }
+        if (ua.includes('OPR')){
+                finalBrowser="Opera";
+        }
+        else if (navigator.brave !== undefined){
+                finalBrowser = "Brave"
+        }
+        else if (ua.includes('Firefox')){
+                finalBrowser="Firefox";
+        }
+        else if (ua.includes('Edg')){
+                finalBrowser="Edge";
+        }
+        else if (ua.includes('Chrome')){
+                finalBrowser="Chrome";
+        }
+        else if (ua.includes('Safari')){
+                finalBrowser="Safari";
+        }
+        document.write("<h1> You are running " + finalBrowser + " on " + finalOs + "</h1>");
 }
 osbrowserfunction();
 let x = localStorage.getItem("visited");
