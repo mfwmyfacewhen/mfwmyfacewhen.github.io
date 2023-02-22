005ea2f0a064b16faa43ff16680448ca75ea2e03
mfwmyfacewhen <94880365+mfwmyfacewhen@users.noreply.github.com>
2023-02-22 04:32:06 -0600

    fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getIP(json) {
         document.getElementById("ip").innerText = "Your public IP address is " + json.ip;
 }
 function ublock() {
-        if (document.getElementById("ip").innerHTML == "")
+        if (document.getElementById("ip").innerText == "")
                 document.getElementById("ublock").innerText = "Are you using UBlock? Naughty naughty";
 }
 function osbrowserfunction () {
