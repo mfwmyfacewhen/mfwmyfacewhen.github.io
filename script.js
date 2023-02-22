c177a775363d799bc1879c13e1a561f95addbeb0
mfwmyfacewhen <94880365+mfwmyfacewhen@users.noreply.github.com>
2023-02-22 02:04:33 -0600

    ublock

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function getIP(json) {
         document.getElementById("ip").innerText = "Your public IP address is " + json.ip;
-}
-function checkUblock() {
-        if (!document.getElementById("ip").innerHTML)
-            document.getElementById("ublock").innerText = "Are you using UBlock? Naughty naughty";
-}
+}();
+if (!document.getElementById("ip").innerHTML)
+    document.getElementById("ublock").innerText = "Are you using UBlock? Naughty naughty";
 function osbrowserfunction () {
         let ua = navigator.userAgent;
         let finalOs = "";
