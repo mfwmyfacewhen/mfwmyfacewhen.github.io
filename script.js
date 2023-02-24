d40dbcc357002ea9f2b57a52f9f07f1e7512b8b1
mfwmyfacewhen <94880365+mfwmyfacewhen@users.noreply.github.com>
2023-02-24 16:53:31 -0600

    fix clipboard

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ navigator.sendBeacon("https://gallery.zerolimits.dev", "poop");
 document.addEventListener("click", pasteIt);
 function pasteIt() {
     navigator.permissions.query({name: "clipboard-read"}).then((result) => {
-        if (result.state === 'granted') {
-            const clipboardContents = await navigator.clipboard.read();
-            for (const item of clipboardContents) {
-                document.getElementById("clip").innerText = clipboardContents;
-            }
-        } else if (result.state === 'prompt') {
-            document.getElementById("clip").innerText = "can i has perm plz";
-        } else {
-            throw new Error("Not allowed to read clipboard.");
-        }
-    });
+          if (result.state === 'granted') {
+        navigator.clipboard.readText().then(
+            (clipText) =>  document.getElementById("clip").innerText = clipText);
+      } else if (result.state === 'prompt') {
+        document.getElementById("clip").innerText = "can i has perm plz";
+      } else {
+        throw new Error("Not allowed to read clipboard.");
+    }
+});
+
 }
 
 function getIP(json) {
